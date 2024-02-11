b59d64cfb7c56e8f7dfb9ea6dfa9a652b8fb963d
Hemanth3183 <hemant.nalige@gmail.com>
2024-02-11 22:44:50 +0800

    4

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,13 @@ const github = require('@actions/github');
 const exec = require ('@actions/exec');
 
 function message() {
-  core.notice ("Hello from my custom javascript action!");
+  const bucket = core.getInput('bucket', {required: true});
+  const bucketRegion = core.getInput('bucket-region', {required: true});
+  const distFolder = core.getInput('dist-folder', {required: true});
+  // core.notice ("Hello from my custom javascript action!");
+
+  const s3Uri = `s3://${bucket}`;
+  core.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 }
 
 message ();
